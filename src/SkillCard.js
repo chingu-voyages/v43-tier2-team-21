a8132390b33928dd18673dbb6558482515544bd5
robert <matthewrueter51@gmail.com>
2023-03-15 12:22:42 -0500

    add rough-draft styles to SkillCard.

--- src/SkillCard.js
+++ src/SkillCard.js
@@ -1,10 +1,12 @@
 export const SkillCard = () =>{
 	return (
-		<div>
-			<h1>Piano</h1>
-			<div>
-				2 / 6
-			</div>
+		<div className={
+			`box-border h-48 w-48 m-2
+			grid content-evenly justify-center
+			border border-black rounded-lg bg-sky-100 shadow-md
+			font-serif`}>
+			<h1 className={`text-3xl`}>Piano</h1>
+			<div className={`text-3xl text-center`}>2 / 6</div>
 		</div>
 	)
 }
