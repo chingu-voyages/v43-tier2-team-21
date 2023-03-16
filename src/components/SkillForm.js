dfef335ef4b85df272be697fe9009b3fbeffdb21
melton-paul <melton.paul@hotmail.com>
2023-03-16 16:36:47 -0500

    SkillForm submit error handling

--- src/components/SkillForm.js
+++ src/components/SkillForm.js
@@ -4,9 +4,9 @@ import InputSection from "./InputSection";
 const initialState = {
   skillName: "",
   skillNameTouched: false,
-  timeGoal: null,
+  timeGoal: 0,
   timeGoalTouched: false,
-  pointGoal: null,
+  pointGoal: 0,
   pointGoalTouched: false,
 };
 
@@ -25,6 +25,10 @@ const SkillForm = ({ prepopulatedData }) => {
     pointGoalTouched,
   } = skillData;
 
+  const skillNameHasError = skillNameTouched && skillName.length === 0;
+  const timeGoalHasError = timeGoalTouched && !timeGoal;
+  const pointGoalHasError = pointGoalTouched && !pointGoal;
+
   const handleChange = (e) => {
     const { value, name } = e.target;
     setSkillData((prev) => ({ ...prev, [name]: value }));
@@ -42,7 +46,7 @@ const SkillForm = ({ prepopulatedData }) => {
       timeGoalTouched: true,
       pointGoalTouched: true,
     }));
-    if (skillNameHasError || timeGoalHasError || pointGoalHasError) {
+    if (!timeGoal || !pointGoal || !skillNameTouched) {
       console.log("Error occured, no submit");
       setTriedSubmit(true);
       return;
@@ -52,10 +56,6 @@ const SkillForm = ({ prepopulatedData }) => {
     // ... push to context array
     setSkillData(initialState);
   };
-
-  const skillNameHasError = skillNameTouched && skillName.length === 0;
-  const timeGoalHasError = timeGoalTouched && !timeGoal;
-  const pointGoalHasError = pointGoalTouched && !pointGoal;
 
   return (
     <form onSubmit={handleSubmit} className="bg-sky-100 p-4">
@@ -75,7 +75,7 @@ const SkillForm = ({ prepopulatedData }) => {
       <InputSection
         name="timeGoal"
         value={timeGoal}
-        label="How much time would you like to practice?"
+        label="How much time would you like to practice a session?"
         hasError={timeGoalHasError}
         changeHandler={handleChange}
         blurHandler={handleBlur}
