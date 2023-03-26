c9ca83cadd249281e994209d0ece521370d0e09e
Tyler Thietje <tylerthietje@gmail.com>
2023-03-26 15:44:38 -0500

    structure of landing page created

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,1 +1,48 @@
-// push test
+const LandingPage = () => {
+
+    return (
+      <>
+        <section class=" flex-col jusify-center bg-red">
+          <h1 class="">Skill Tracker</h1>
+          <h2>Start Learning Something New Today!</h2>
+          <button>Sign Up Now!</button>
+        </section>
+
+        <section>
+          <div>
+            <h2>Feature</h2>
+            <p>Feature Description</p>
+          </div>
+          <div>
+            <h2>Feature</h2>
+            <p>Feature Description</p>
+          </div>
+          <div>
+            <h2>Feature</h2>
+            <p>Feature Description</p>
+          </div>
+        </section>
+        
+        <section>
+          <h2>Testimonials</h2>
+          <div>
+            <h3>Testimonial Title</h3>
+            <p>Testimonial Description</p>
+            <p>Testimonial Author</p>
+          </div>
+          <div>
+            <h3>Testimonial Title</h3>
+            <p>Testimonial Description</p>
+            <p>Testimonial Author</p>
+          </div>
+          <div>
+            <h3>Testimonial Title</h3>
+            <p>Testimonial Description</p>
+            <p>Testimonial Author</p>
+          </div>
+        </section>
+      </>
+    );
+}
+
+export default LandingPage;
