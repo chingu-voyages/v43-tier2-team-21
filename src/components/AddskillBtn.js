22ac04cfc5c9024641ecb5ed097baad039d9f82e
robert <matthewrueter51@gmail.com>
2023-03-27 14:38:41 -0500

    Create add skill button.

---
+++ src/components/AddskillBtn.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export const AddskillBtn = () =>{
+	return <Link to={"/edit"}>	
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="fill-blue-200 w-14 h-14">
+		<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+		</svg>
+	</Link>
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,11 @@
-import SkillForm from "../components/SkillForm";
 import { SkillCard } from "../components/SkillCard";
-import Timer from "../components/Timer";
 import DueToday from "../components/DueToday/DueToday";
+import { AddskillBtn } from "../components/AddskillBtn";
 
 const Dashboard = () => {
   return (
     <>
+      <AddskillBtn />
       <SkillCard />
       <DueToday />
     </>
