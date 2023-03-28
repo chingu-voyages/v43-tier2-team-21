f293d448b7c0e7f70d70db94191cb448968fdc68
melton-paul <melton.paul@hotmail.com>
2023-03-28 15:45:37 -0500

    Unified styling

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,39 +1,65 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-
-export const SkillCard = ({skillData}) => {
-  const {skillName, goal, completedSessions} = skillData
+export const SkillCard = ({ skillData }) => {
+  const { skillName, goal, completedSessions } = skillData;
 
   return (
     <div
       onClick={() => console.log("clicked")}
-      className={`box-border h-48 w-48 m-2
+      className={`box-border h-60 w-60 m-2
 				grid content-evenly justify-center
 				border border-black rounded-lg bg-sky-100 shadow-md
 				font-serif`}
     >
       <h1 className={`text-3xl text-center`}>{skillName}</h1>
-      <div className={`text-3xl text-center`}>{goal} / {completedSessions}</div>
+      <div className={`text-3xl text-center`}>
+        {goal} / {completedSessions}
+      </div>
       <div className={`h-8 w-48 flex justify-evenly`}>
-        <Link to={"/edit"}><EditButton/></Link>
-        <Link to={"/timer"}><StartButton/></Link>
+        <Link to={"/edit"}>
+          <EditButton />
+        </Link>
+        <Link to={"/timer"}>
+          <StartButton />
+        </Link>
       </div>
     </div>
   );
 };
 
-
-const EditButton = () =>{
-  return(
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-    </svg>
-  )
-}
-const StartButton = () =>{
+const EditButton = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+      />
     </svg>
-  )
-}
+  );
+};
+const StartButton = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+      />
+    </svg>
+  );
+};
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,19 +1,29 @@
+import React from "react";
 import DueToday from "../components/DueToday/DueToday";
-import { AddskillBtn } from "../components/AddskillBtn";
-import { SkillCardContainer} from "../components/SkillCardContainer"
+import { SkillCardContainer } from "../components/SkillCardContainer";
 import EmptyModal from "../components/Modals/EmptyModal";
 import CreateSkillModal from "../components/Modals/CreateSkillModal";
 
-
 const Dashboard = () => {
+  const [isAdding, setIsAdding] = React.useState(false);
+
+  const containerSectionStyle = "my-10 w-full";
+
+  function toggleAdding() {
+    setIsAdding((prev) => !prev);
+  }
+
   return (
-    <>
-      <AddskillBtn />
-      <SkillCardContainer />
-      <DueToday />
+    <div className="flex flex-col items-center max-w-5xl mx-auto">
+      <div className={containerSectionStyle}>
+        <SkillCardContainer toggleAdding={toggleAdding} />
+      </div>
+      <div className={containerSectionStyle}>
+        <DueToday />
+      </div>
+      {isAdding && <CreateSkillModal toggleAdding={toggleAdding} />}
       <EmptyModal />
-      <CreateSkillModal />
-    </>
+    </div>
   );
 };
 
