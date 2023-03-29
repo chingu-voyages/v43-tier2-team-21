1148230e41e38cae9e2c8ca1112a5257fa51667a
Tyler Thietje <tylerthietje@gmail.com>
2023-03-29 18:51:10 -0500

    created NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between items-center font-sans text-xl p-2 bg-">
+      <div className="logo text-4xl font-extrabold">Skill Tracker</div>
+      <div className="links">
+        <ul className="flex gap-4 pr-3 font-light">
+          <li>
+            <Link to={'/dashboard'}>Sign Up!</Link>
+          </li>
+          <li>
+            <Link to={'/dashboard'}>Log In</Link>
+          </li>
+          <li>
+            <Link to={'/addskill'}>Add Skill</Link>
+          </li>
+          <li>
+            <Link to={'/edit'}>Edit Goals</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
