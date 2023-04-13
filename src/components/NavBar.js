d657361805ac80c205c52c0d4d578704ef9e1f0d
Tyler Thietje <tylerthietje@gmail.com>
2023-04-13 14:39:55 -0500

    finished navbar responsive styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,23 +14,34 @@ const NavBar = ({ toggleAdding }) => {
   }
 
   return (
-    <nav className="flex justify-between items-center font-sans text-xl p-2 bg-">
-      <Link to="/dashboard" className="logo text-4xl font-extrabold">
+    <nav className="flex justify-between items-center text-xl p-4">
+      <Link
+        to="/dashboard"
+        className="sm:text-xl logo md:text-4xl font-extrabold"
+      >
         Skill Tracker
       </Link>
       <div className="links">
         <ul className="flex gap-4 pr-3 font-light items-center">
           {!authCtx.isLoggedIn ? (
             <li>
-              <Link to={"/auth"}>Log In</Link>
+              <Link to={'/auth'}>Log In</Link>
             </li>
           ) : (
             <li>
-              <Button onClick={authCtx.logout}>Log Out</Button>
+              <Button
+                className="sm:text-base md:text-lg"
+                onClick={authCtx.logout}
+              >
+                Log Out
+              </Button>
             </li>
           )}
           <li>
-            <Button className="ml-0 bg-sky-100" onClick={handleAdding}>
+            <Button
+              className="sm:text-base md:text-lg bg-purple-200"
+              onClick={handleAdding}
+            >
               Add Skill
             </Button>
           </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      'sm': '350px',
+      // => @media (min-width: 350px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
