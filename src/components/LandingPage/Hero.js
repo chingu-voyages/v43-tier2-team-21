9285320361e5fc32034b481cfe5fc91eb9b73b45
robert <matthewrueter51@gmail.com>
2023-04-14 11:10:01 -0500

    wire up sign-up buttons to AuthForm.

--- src/components/LandingPage/Hero.js
+++ src/components/LandingPage/Hero.js
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import heroImage from "../../images/skills2.png"
+
 
 const Hero = () => {
   return (
@@ -6,12 +8,9 @@ const Hero = () => {
       <img src={heroImage} alt="" className="w-56" />
       <h1 className="text-6xl pt-20 font-bold">Skill Tracker</h1>
       <h2 className="text-2xl">Start Learning Something New Today!</h2>
-      <button
-        className="justify-center cursor-pointer px-5 py-2 my-10 rounded-3xl text-lg bg-white text-purple-600 hover:bg-gray-500 hover:text-black active:bg-gray-600"
-        onClick={() => console.log("button clicked")} // Connect to sign-in modal
-      >
-        Sign Up Now!
-      </button>
+        <Link to={"/auth"} state={{isLoggingIn:false}}className="justify-center cursor-pointer px-5 py-2 my-10 rounded-3xl text-lg bg-white text-purple-600 hover:bg-gray-500 hover:text-black active:bg-gray-600">
+          Sign Up Now!
+        </Link>      
     </section>
   );
 };
