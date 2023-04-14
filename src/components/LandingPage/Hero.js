3da09cd977443b2070b36096d5eadd96ec9673a6
robert <matthewrueter51@gmail.com>
2023-04-14 18:45:28 -0500

    re-wire sign up now button on LandingPage/Hero.

--- src/components/LandingPage/Hero.js
+++ src/components/LandingPage/Hero.js
@@ -8,12 +8,9 @@ const Hero = () => {
       <img src={heroImage} alt="" className="w-48 lg:w-56" />
       <h1 className="text-4xl lg:text-6xl pt-20 font-bold">Skill Tracker</h1>
       <h2 className="text-lg lg:text-2xl px-10">Start Learning Something New Today!</h2>
-      <button
-        className="justify-center cursor-pointer px-5 py-2 my-10 rounded-3xl text-lg bg-white text-purple-600 hover:bg-gray-500 hover:text-black active:bg-gray-600"
-        onClick={() => console.log("button clicked")} // Connect to sign-in modal
-      >
+      <Link to={"/auth"} state={{isLoggingIn:false}} className="justify-center cursor-pointer px-5 py-2 my-10 rounded-3xl text-lg bg-white text-purple-600 hover:bg-gray-500 hover:text-black active:bg-gray-600">
         Sign Up Now!
-      </button>
+      </Link>
     </section>
   );
 };
